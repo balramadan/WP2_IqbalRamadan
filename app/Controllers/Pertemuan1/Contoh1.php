22f6c22432f398b8cc101ba7d2b2f899957ef8b9
Iqbal Ramadan <balramadan@hotmail.com>
2024-03-07 09:44:33 +0700

    Initial commit

---
+++ app/Controllers/Pertemuan1/Contoh1.php
@@ -0,0 +1,17 @@
+<?php
+// defined('BASEPATH') OR exit('No direct script access allowed');
+
+namespace App\Controllers\Pertemuan1;
+
+use App\Controllers\BaseController;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Iqbal Ramadan. 
+        Saya tinggal di daerah Kebon Jeruk. 
+        Olahraga yang saya sukai adalah Sepak Bola";
+    }
+}
