255897de08a95d59c75e37817da808df2955e6ee
Iqbal Ramadan <iqbalramadan3130@gmail.com>
2024-03-07 10:12:46 +0700

    nambahin Pertemuan 1

---
+++ Pertemuan1/app/Controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+// defined('BASEPATH') OR exit('No direct script access allowed');
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+}
